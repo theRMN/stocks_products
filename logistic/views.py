260434fe48d13d98a9ad1ec75b2198ca92c8e6ed
theRMN <roman.vassev@gmail.com>
2021-07-12 18:12:59 +0600

    fixed filters

--- logistic/views.py
+++ logistic/views.py
@@ -16,7 +16,7 @@ class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
     filter_backends = [SearchFilter]
-    search_fields = ['products__id']
+    search_fields = ['products__title', 'products__description', 'products__id']
 
 
 class StockProductViewSet(ModelViewSet):
